586e2564f875ac2f8a737e7646394ad5a15f7854
marko <81442@alumnos.utleon.edu.mx>
2025-09-24 18:21:49 -0600

    xd

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,12 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
-import ReloadPrompt from './ReloadPrompt.jsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.jsx";
+import ReloadPrompt from "./ReloadPrompt.jsx";
 
-createRoot(document.getElementById('root')).render(
+createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <ReloadPrompt/>
+    <ReloadPrompt></ReloadPrompt>
     <App />
-  </StrictMode>,
-)
+  </StrictMode>
+);
